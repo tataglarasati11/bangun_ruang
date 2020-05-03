53e5b4c371aa0cdcd2cac9a4211ffbedc6d6dd4e
tataglarasati11 <60909380+tataglarasati11@users.noreply.github.com>
2020-05-03 14:09:41 +0700

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -6,51 +6,50 @@
 using namespace std;
 
 void header(){
-	cout<<"====================================="<<endl;
-	cout<<"menghitung Volume Kerucut dan Tabung "<<endl;
-	cout<<"====================================="<<endl;
+   cout<<"====================================="<<endl;
+   cout<<"menghitung Volume Kerucut dan Tabung "<<endl;
+   cout<<"====================================="<<endl;
 }
 
 int main()
 {
-system("cls");
-tabung bung;
-kerucut cut;
+  system("cls");
+  tabung bung;
+  kerucut cut;
 
-bangun_ruang_sisi_lengkung *list[1];
-list[1]=&bung;
-bangun_ruang_sisi_lancip *poin[1];
-poin[1]=&cut;
+    bangun_ruang_sisi_lengkung *list[1];
+    list[1]=&bung;
+    bangun_ruang_sisi_lancip *poin[1];
+    poin[1]=&cut;
 
 	int pilih;
 	char ulang;
 	do{
-		system("cls");
-		  header();
-		  cout<<"1) Tabung\n";
-		  cout<<"2) Kerucut\n";
-		  cout<<"masukkan pilihan:";
-		  cin>>pilih;
-		  
-		  if(pilih==1){
-		          list[1]->get_data();
-		          list[1]->display_area();
-		          getch();
-		          }
-		  else if (pilih==2){
-		          poin[1]->get_data();
-		          poin[1]->display_area();
-		          getch();
-		          }
-		 else{
-		     cout <<"maaf pilihan yang anda masukkan salah"<<endl;
-		     getch();
-			}
-		cout << "apakaah ingin menghitung lagi ? "<<endl ;
-		cout << "Y/T"<<endl;
-		cout << " pilih : ";
-		cin >> ulang;	
-		system("cls");
+	  system("cls");
+	  header();
+	  cout<<"1) Tabung\n";
+	  cout<<"2) Kerucut\n";
+	  cout<<"masukkan pilihan:";
+	  cin>>pilih;	  
+	        if(pilih==1){
+		   list[1]->get_data();
+		   list[1]->display_area();
+	   	   getch();
+	   	   }
+	   else if (pilih==2){
+		   poin[1]->get_data();
+		   poin[1]->display_area();
+		   getch();
+	           }
+	   else{
+		   cout <<"maaf pilihan yang anda masukkan salah"<<endl;
+		   getch();
+		    }
+	   cout << "apakaah ingin menghitung lagi ? "<<endl ;
+	   cout << "Y/T"<<endl;
+	   cout << " pilih : ";
+	   cin >> ulang;	
+	   system("cls");
 	}while(ulang=='Y'||ulang=='y');
 	cout << "Terima kasih telah menggunakan program saya untuk menghitung"<<endl<<endl;
 	getch ();
@@ -59,4 +58,4 @@ poin[1]=&cut;
 	cout << " D04217025"<<endl;
     cout << "=============================="<<endl;	
     return 0;
-}
+ }
